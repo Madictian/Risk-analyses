d8f7201113428805b613a05d0fb77f30b0a14ffd
Sim0n <57576661+simon-stentoft@users.noreply.github.com>
2020-12-10 13:12:14 +0100

    Made Risk class

---
+++ src/main/RiskAnalysis.java
@@ -0,0 +1,10 @@
+package main;
+
+import java.util.ArrayList;
+
+public class RiskAnalysis {
+    int ID;
+    int userID;
+    String title;
+    ArrayList<Risk> riskArrayList = new ArrayList<Risk>();
+}
