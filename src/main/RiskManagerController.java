fd725a2829b76d54e3c270c3de915516b58d8b44
Madictian <fredrikbille@hotmail.com>
2020-12-10 14:04:49 +0100

    Creation of RiskManagerController

--- src/main/RiskManagerController.java
+++ src/main/RiskManagerController.java
@@ -1,11 +1,11 @@
 package main;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.*;
 import javafx.scene.input.InputEvent;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 
-import java.awt.*;
 import java.util.ArrayList;
 
 public class RiskManagerController {
