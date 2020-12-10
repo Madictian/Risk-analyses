0b37b076279b3934a8063ade253e0b43caab7951
Sim0n <57576661+simon-stentoft@users.noreply.github.com>
2020-12-10 12:51:06 +0100

    Made Risk class

---
+++ src/main/Risk.java
@@ -0,0 +1,11 @@
+package main;
+
+public class Risk {
+    String description;
+    String probability;
+    String consequence;
+    int priority;
+    String revisedProbability;
+    String revisedConsequence;
+    String counterMeasure;
+}
