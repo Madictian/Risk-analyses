3eb62963837a24ce361265d90da1015b82561477
Madictian <fredrikbille@hotmail.com>
2020-12-11 10:43:42 +0100

    Making changes to accommodate private data types

--- src/main/RiskManagerController.java
+++ src/main/RiskManagerController.java
@@ -20,6 +20,7 @@ public class RiskManagerController {
 
     // GUI change
     enum ChangeType {
+        DASHBOARD,
         RISKANALYSIS,
         RISK;
     }
@@ -51,17 +52,22 @@ public class RiskManagerController {
     private void updateGUI(ChangeType type) {
 
         switch (type) {
-            case RISKANALYSIS -> {
+            case DASHBOARD -> {
 
             }
+            case RISKANALYSIS -> {
+                for (int i = 0; i < rA.getRiskArrayList().size(); i++) {
+                    // TODO Create in GUI the risks in a table formation
+                }
+            }
             case RISK -> {
-                riskDescription.setText(currentRisk.description);
-                riskProbability.setText(currentRisk.probability);
-                riskConsequence.setText(currentRisk.consequence);
-                riskPriority.setText(Integer.toString(currentRisk.priority));
-                riskRevisedProbability.setText(currentRisk.revisedProbability);
-                riskRevisedConsequence.setText(currentRisk.revisedConsequence);
-                riskCountermeasure.setText(currentRisk.counterMeasure);
+                riskDescription.setText(currentRisk.getDescription());
+                riskProbability.setText(currentRisk.getProbability());
+                riskConsequence.setText(currentRisk.getConsequence());
+                riskPriority.setText(Integer.toString(currentRisk.getPriority()));
+                riskRevisedProbability.setText(currentRisk.getRevisedProbability());
+                riskRevisedConsequence.setText(currentRisk.getRevisedConsequence());
+                riskCountermeasure.setText(currentRisk.getCounterMeasure());
             }
         }
 
@@ -70,26 +76,27 @@ public class RiskManagerController {
 
     // Handles all risk analysis matters
     public void createRiskAnalysis(String name) {
-
+        updateGUI(ChangeType.RISKANALYSIS);
         RiskAnalysis newRiskAnalysis = new RiskAnalysis();
         // TODO
     }
     public void editRiskAnalaysis(RiskAnalysis rA) {
-
+        updateGUI(ChangeType.RISKANALYSIS);
 
         // TODO
     }
     public void deleteRiskAnalysis(RiskAnalysis rA) {
-
+        updateGUI(ChangeType.RISKANALYSIS);
         // TODO
     }
     public void saveRiskAnalysis(RiskAnalysis rA) {
+        updateGUI(ChangeType.DASHBOARD);
         // TODO
     }
 
     // Handles all risk matters
     public void editRisk() {
-
+        updateGUI(ChangeType.RISK);
         // TODO find out a way to set r to the current risk open in GUI
 
         // Call this when you exit the risk you were editing
@@ -147,7 +154,13 @@ public class RiskManagerController {
         // TODO
     }
 
-    public void redoText() {
+    public void redoText(KeyEvent event1, KeyEvent event2) {
+
+        if (event1.getCode() == KeyCode.CONTROL && event2.getCode() == KeyCode.Y) {
+
+
+            updateGUI(ChangeType.RISK);
+        }
         // TODO
     }
 
