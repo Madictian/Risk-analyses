7672a4ff7652e3b5359956afc5dd9d29140c6661
Sim0n <57576661+simon-stentoft@users.noreply.github.com>
2020-12-11 11:07:17 +0100

    Updated Risk and Riskanalysis

--- src/main/RiskAnalysis.java
+++ src/main/RiskAnalysis.java
@@ -3,8 +3,43 @@ package main;
 import java.util.ArrayList;
 
 public class RiskAnalysis {
-    int ID;
-    int userID;
-    String title;
-    ArrayList<Risk> riskArrayList = new ArrayList<Risk>();
+    private int ID;
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void add(Risk risk) {
+        riskArrayList.add(risk);
+    }
+    public void remove(Risk risk) {
+        riskArrayList.remove(risk);
+    }
+    public boolean isEmpty() {
+        return riskArrayList.isEmpty();
+    }
+
+    private int userID;
+    private String title;
+    private ArrayList<Risk> riskArrayList = new ArrayList<Risk>();
 }
