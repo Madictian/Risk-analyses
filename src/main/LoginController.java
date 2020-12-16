8ba01fc8de6ec233dff53990a5ac28f99d44a495
virtualzeth <virtualzeth@gmail.com>
2020-12-16 16:21:34 +0100

    navigation update

--- src/main/LoginController.java
+++ src/main/LoginController.java
@@ -3,10 +3,17 @@ package main;
 import database.DatabaseHandler;
 
 import database.User;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
@@ -20,7 +27,16 @@ public class LoginController implements Initializable {
     @FXML
     private PasswordField passwordInputField;
 
-    public void login() throws InvalidKeySpecException, SQLException, NoSuchAlgorithmException {
+    public void changeScreenToRiskManager(ActionEvent e) throws IOException {
+        Parent parent = FXMLLoader.load(getClass().getResource("riskManagerView.fxml"));
+        Scene scene = new Scene(parent);
+
+        Stage window = (Stage) ((Node)e.getSource()).getScene().getWindow();
+        window.setScene(scene);
+        window.show();
+    }
+
+    public void login(ActionEvent e) throws InvalidKeySpecException, SQLException, NoSuchAlgorithmException, IOException {
         String username = usernameInputField.getText();
         String password = passwordInputField.getText();
 
@@ -28,6 +44,8 @@ public class LoginController implements Initializable {
         User user = DatabaseHandler.validateUser(conn, username, password);
         if(user == null) {
             System.out.println("Invalid user");
+        } else {
+            changeScreenToRiskManager(e);
         }
     }
 
